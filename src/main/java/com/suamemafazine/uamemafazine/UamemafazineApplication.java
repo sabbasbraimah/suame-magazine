f79eb8b0716fe8a3816cf41774c33c9b48d331f2
Seidu Abbas <braimah_abbas@yahoo.co.uk>
2022-03-05 22:00:09 +0000

    First Magazine commit

---
+++ src/main/java/com/suamemafazine/uamemafazine/UamemafazineApplication.java
@@ -0,0 +1,13 @@
+package com.suamemafazine.uamemafazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UamemafazineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UamemafazineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suamemafazine/uamemafazine/UamemafazineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suamemafazine.uamemafazine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UamemafazineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
